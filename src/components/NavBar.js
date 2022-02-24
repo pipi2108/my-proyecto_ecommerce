4a68bddeedad8363c41951c4274e7814bfac6a60
gastonl <glucero_96@hotmail.com>
2022-02-23 22:44:49 -0300

    se creo navbar para desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar() {
+    return (
+        <header id="my-header">
+            <div>
+                <h2> Caprichitos </h2>
+            </div>
+            <div>
+                <a href="#"> Quienes Somos </a>
+                <a href="#"> Productos </a>
+                <a href="#"> Ofertas </a>
+                <a href="#"> Contacto </a>
+            </div>
+        </header>
+        )
+}
+
+export default NavBar
